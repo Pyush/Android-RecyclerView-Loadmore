e9d95b56da43045062daefed45585c5a40590cd5
Piyush <piyushpatel.love6@gmail.com>
2017-01-10 14:44:10 +0530

    Initial Commit

---
+++ app/src/main/java/com/recyclerview/loadmore/MainActivity.java
@@ -0,0 +1,13 @@
+package com.recyclerview.loadmore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
