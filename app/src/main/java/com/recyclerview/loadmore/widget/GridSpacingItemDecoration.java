e92fc939eb8bb0f709bc41dc60eae975c3a31f77
Neo <c.min.tseng@gmail.com>
2022-06-11 02:51:31 +0800

    Migrate to AndroidX

--- app/src/main/java/com/recyclerview/loadmore/widget/GridSpacingItemDecoration.java
+++ app/src/main/java/com/recyclerview/loadmore/widget/GridSpacingItemDecoration.java
@@ -25,7 +25,7 @@ package com.recyclerview.loadmore.widget;
  */
 
 import android.graphics.Rect;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 
 /**
